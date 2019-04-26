d3b48f1b21e352667469838624fd0a3e4bc81baa
wfpcoder <18368164173@163.com>
2019-04-26 13:48:11 +0800

    initial commit

---
+++ out/artifacts/itheima_JavaWeb_war_exploded/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/4/18 0018
+  Time: 16:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>QQ登录</title>
+  </head>
+  <body>
+    用户名:<input type="text" align="center" id="uname" placeholder="请输入用户名"/><br/>
+    密码:<input type="password" align="center" id="password" placeholder="请输入密码"/>
+    a<img src="WEB-INF/img/">
+  </body>
+</html>
---
+++ src/com/itheima/web/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.itheima.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().write("hello china");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+}
